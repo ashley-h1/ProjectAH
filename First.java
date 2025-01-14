7a4ff8ad00a3e067b9dde343b4593def53f13e02
Ashley Huff <huffa0207@gmail.com>
2025-01-13 21:21:36 -0500

    Our first commit, assignment 1

---
+++ First.java
@@ -0,0 +1,9 @@
+// Ashley Huff
+public class First{
+    public static void main(String args[]){
+        System.out.println("Hola mundo");
+        System.out.println("Job Title: Information Security Analysts");
+        System.out.println("Why: Good salary and outlook");
+        System.out.println("Median Pay: $120,360");
+    }
+}
